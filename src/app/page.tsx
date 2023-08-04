ec9498eeb0d54edd1cf44df43c63cf11238e06ca
MD. Shimul <mdshemul480@gmail.com>
2023-08-04 08:18:18 +0600

    adding section divider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from "@/components/Intro/Intro";
+import { SectionDivider } from "@/components/SectionDivider/SectionDivider";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center">
       <Intro />
+      <SectionDivider />
     </main>
   );
 }
