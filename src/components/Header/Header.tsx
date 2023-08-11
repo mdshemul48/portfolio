7a77b8c80c49968ce9b2fdd13a3e328bac1dec67
MD. Shimul <mdshemul480@gmail.com>
2023-08-11 16:03:02 +0600

    adding animation to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { links } from "@/lib/data";
+import clsx from "clsx";
 
 export default function Header() {
+  const [actionSection, setActionSection] = useState("Home");
+
   return (
     <header className="z-[999] relative">
       <motion.div
@@ -21,13 +24,30 @@ export default function Header() {
               initial={{ y: -100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               key={link.hash}
-              className="h-3/4 flex items-center justify-center "
+              className="h-3/4 flex items-center justify-center relative"
             >
               <Link
                 href={link.hash}
-                className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition"
+                className={clsx(
+                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition",
+                  {
+                    "text-gray-950": actionSection === link.name,
+                  }
+                )}
+                onClick={() => setActionSection(link.name)}
               >
                 {link.name}
+                {link.name === actionSection && (
+                  <motion.span
+                    layoutId="actionSection"
+                    transition={{
+                      type: "spring",
+                      stiffness: 380,
+                      damping: 30,
+                    }}
+                    className="bg-gray-100 rounded-full absolute inset-0 -z-10"
+                  />
+                )}
               </Link>
             </motion.li>
           ))}
